609a15cb3fbb8451b8cd35792f9a1288841f453a
turi <crankyyrabbit@gmail.com>
2022-01-09 22:21:31 +0900

    turi: More text 페이지 조정

--- src/components/More.js
+++ src/components/More.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import '../stylesheets/Signin.css';
+import '../stylesheets/More.css';
 import { TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
 
@@ -13,26 +13,65 @@ function More(){
     const [result, setResult] = useState("");
     const onSubmit = (data) => setResult(JSON.stringify(data));
   
-    // className='termOfUSe' 와 className='policy' 의 링크 분리 코드 검색 
+    // 1) className='textOfUSe' 와 className='textOfPolicy' 의 -> 같은 링크, 다른 페이지 해결 하기 
+    // 2) 스크롤바 모양 변경 하기 
+    // 3) NavBar fontsize 변경 하기 
     return (
 
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className='formOfText'>
     
-      <div className='termOfUse'> {/* div 넓이 및 폰트 조정 */}
+      <div className='textOfUse'>
           
         "todo list" Term of Use <br />
         <br />
         "todo list" and "todo list" related to the use of serviced related to the requirements are defined. <br />
         <br />
-        Article 1 (Purpose) <br />
-        These Terms and Conditions shall be governed by the "todo list"and the rights and dutied of Members in
-        connection with the use of the "todo list" and "todo list" serviced provided by "todo list" And
-        responsibilities, and other necessary matters.
+        This Privacy Policy (the "Policy") explains the way of treatment of the information which is 
+        provided or collected in the websites on which this Policy is posted. <br />
+        <br />
+        In addition the Policy also explains the information which is provided or collected in the course of using the applications 
+        of the Company which exist in the websites or platforms of other company. <br /> 
+        <br />
+        Through this Policy, the Company regards personal information of the users as important and inform them of the purpose and method of Company's using the personal information 
+        provided by the users and the measures taken by the Company for protection of those personal information. <br />
+        <br />
+        This Policy will be effective on the ___th day of ____, 2015 and, in case of modification thereof, the Company will make public notice of it through posting it on the bulletin board of 
+        Company's website or individual notice through sending mails, fax or e-mails). <br />
+        <br />
+        1. Information to be collected and method of collection <br />
+        <br />
+        ① Personal information items to be collected <br />
+        Personal information items to be collected by the Company are as follows: <br />
+        • Information provided by the users <br />
+        The Company may collect the information directly provided by the users. <br />
+        [Option to select 'personal information items' in Appendix '1-1' of Personal Privacy Policy] <br />
+        • Information collected while the users use services <br />
+        Besides of information directly provided by the users, the Company may collect information in the course that the users use the service provided by the Company. <br />
+        [Option to select 'personal information items' in Appendix '1-2' of Personal Privacy Policy] <br />
+        <br />
+        ② Method of collection <br />
+        The Company collects the information of users in a way of the followings: <br />
+        [Option to select 'method of collection' in Appendix '2' of Personal Privacy Policy] <br />
+        <br />
+        2. Use of collected information <br />
+        The Company uses the collected information of users for the following purposes: <br />
+        [Option to select 'use of collected information' in Appendix '3' of Personal Privacy Policy] <br />
+        The Company agrees that it will obtain consent from the users, if the Company desires to use the information other than those expressly stated in this Policy. <br />
+        <br />
+        3. Sharing collected information <br />
+        Except for the following cases, the Company will not share personal information with a 3rd party: <br />
+        • when the Company shares the information with its affiliates, partners and service providers; <br />
+        [Option to select 'sharing of collected information' in Appendix '4-1' of Personal Privacy Policy] <br />
+        <br />
+        • when the users consent the sharing in advance; <br />
+        [Option to select 'sharing of collected information' in Appendix '4-2' of Personal Privacy Policy] <br />
+        <br />
+        • when the sharing is required by the laws <br />
+        - if required to be disclosed by the laws and regulations; or <br />
+        - if required to be disclosed by the investigative agencies for detecting crimes in accordance with the procedure and method as prescribed in the laws and regulations <br />
+        <br />
+        End.
 
-          
-          <p>가입 시 다음 사항에 동의하는 것으로 간주합니다. &nbsp;
-          <Link to="/text" className='termOfUse'>이용약관</Link>과 <Link className='policy' to="/text">개인정보 정책</Link>
-          </p>
       </div>
       </form>
       )
